01c85bb4d6696e05c0eb5ebc355e7bff483c2f26
takeokunn <bararararatty@gmail.com>
2023-08-12 18:46:39 +0900

    Fix bug

--- src/chunk.ts
+++ src/chunk.ts
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import SimplexNoise from 'simplex-noise'
+import { NoiseFunction2D } from 'simplex-noise'
 
 import { BLOCK, TERRIAN } from '@src/constant'
 import { Dart, Grass, BlockInterface } from '@src/blocks'
@@ -11,14 +11,14 @@ interface ChunkInterface {
 class Chunk implements ChunkInterface {
   public blocks: BlockInterface[] = []
 
-  constructor(simplex: SimplexNoise, centerX: number, centerZ: number) {
+  constructor(noise2D: NoiseFunction2D, centerX: number, centerZ: number) {
     for (let x = 0; x < TERRIAN.CHUNK_SIZE; x++) {
       for (let z = 0; z < TERRIAN.CHUNK_SIZE; z++) {
         const positionX = centerX + x
         const positionZ = centerZ + z
         const xoff = TERRIAN.INCREMENT_OFFSET * positionX
         const zoff = TERRIAN.INCREMENT_OFFSET * positionZ
-        const y = Math.round((simplex.noise2D(xoff, zoff) * TERRIAN.AMPLITUDE) / BLOCK.SIZE)
+        const y = Math.round((noise2D(xoff, zoff) * TERRIAN.AMPLITUDE) / BLOCK.SIZE)
         this.blocks.push(
           new Grass(new THREE.Vector3(positionX * BLOCK.SIZE, y * BLOCK.SIZE, positionZ * BLOCK.SIZE), true),
         )
