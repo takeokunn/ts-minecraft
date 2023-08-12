68f065ba67eeb68beec5bf08552a075b86f86f09
takeokunn <bararararatty@gmail.com>
2023-08-13 02:35:49 +0900

    Refactor controller

---
+++ src/controller.ts
@@ -0,0 +1,31 @@
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls'
+
+import { Camera } from '@src/camera'
+
+interface ControllerInterface {
+  moveForward: (distance: number) => void
+  moveRight: (distance: number) => void
+}
+
+class Controller implements ControllerInterface {
+  private pointerLock: PointerLockControls
+  private isLock: boolean = false
+
+  constructor(camera: Camera) {
+    this.pointerLock = new PointerLockControls(camera.perspective, document.body)
+    document.body.addEventListener('click', () => {
+      this.isLock = !this.isLock
+      this.isLock ? this.pointerLock.lock() : this.pointerLock.unlock()
+    })
+  }
+
+  public moveForward(distance: number): void {
+    this.pointerLock.moveForward(distance)
+  }
+
+  public moveRight(distance: number): void {
+    this.pointerLock.moveRight(distance)
+  }
+}
+
+export { Controller, ControllerInterface }
