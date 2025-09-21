f4ffc0acbf2b74d215de10905d50320084fbad12
takeokunn <bararararatty+nixos-container@gmail.com>
2025-09-21 12:25:09 +0900

    Update

--- src/domain/camera/index.ts
+++ src/domain/camera/index.ts
@@ -2,47 +2,26 @@
 export {
   // CameraService
   CameraService,
-  CameraServiceLive,
-  CameraServiceTest,
   CameraMode,
+  CameraConfig,
   CameraState,
-  CameraUpdateParams,
   CameraError,
-  defaultCameraState,
+  DEFAULT_CAMERA_CONFIG,
 } from './CameraService'
 
 export {
   // FirstPersonCamera
-  FirstPersonCamera,
   FirstPersonCameraLive,
-  FirstPersonCameraTest,
-  FirstPersonConfig,
-  PlayerState,
-  defaultFirstPersonConfig,
 } from './FirstPersonCamera'
 
 export {
   // ThirdPersonCamera
-  ThirdPersonCamera,
   ThirdPersonCameraLive,
-  ThirdPersonCameraTest,
-  ThirdPersonConfig,
-  TargetState,
-  CollisionInfo,
-  defaultThirdPersonConfig,
 } from './ThirdPersonCamera'
 
 // 統合Layer（本番用）
 import { Layer } from 'effect'
-import { CameraServiceLive } from './CameraService'
 import { FirstPersonCameraLive } from './FirstPersonCamera'
 import { ThirdPersonCameraLive } from './ThirdPersonCamera'
 
-export const CameraSystemLive = Layer.mergeAll(CameraServiceLive, FirstPersonCameraLive, ThirdPersonCameraLive)
-
-// 統合Layer（テスト用）
-import { CameraServiceTest } from './CameraService'
-import { FirstPersonCameraTest } from './FirstPersonCamera'
-import { ThirdPersonCameraTest } from './ThirdPersonCamera'
-
-export const CameraSystemTest = Layer.mergeAll(CameraServiceTest, FirstPersonCameraTest, ThirdPersonCameraTest)
+export const CameraSystemLive = Layer.mergeAll(FirstPersonCameraLive, ThirdPersonCameraLive)
